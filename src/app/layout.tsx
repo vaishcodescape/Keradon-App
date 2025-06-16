619fee681b83a2a381210ea30331e58153504ac5
Aditya Vaish <adityavaish846@gmail.com>
2025-06-15 18:24:28 -0700

    changes made to repo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Suspense } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,8 +28,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
-      </body>
+           {children}
+       </body>
     </html>
   );
 }
