d918c072200506b40193baac02f15b4cf5f2e9e1
Aditya Vaish <adityavaish846@gmail.com>
2025-06-16 18:48:14 -0700

    Next.js server side api added

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { UserService } from '@/server/models/User';
+import { UserService } from '@/lib/models/User';
 
 export async function POST() {
   try {
